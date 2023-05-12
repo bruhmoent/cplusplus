c849f692f5d4220e888d5c0cfdcad683788c8f49
Rob <69918580+bruhmoent@users.noreply.github.com>
2023-05-12 14:08:13 +0200

    Update element.hpp

--- ships/element.hpp
+++ ships/element.hpp
@@ -5,14 +5,15 @@
 #ifndef ELEMENT_HPP
 #define ELEMENT_HPP
 
-
 struct 
 Element
 {
+
     std::vector<std::pair<int, int>> elementCoordinates;
     int health;
 
     Element(std::vector<std::pair<int, int>> coordinates, int hlth) : elementCoordinates(coordinates), health(hlth){}
+
 };
 
 #endif
