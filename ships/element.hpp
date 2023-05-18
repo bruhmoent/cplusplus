31f3fe0a9fd055fee591ec93333c7ea303493c01
Rob <69918580+bruhmoent@users.noreply.github.com>
2023-05-18 18:37:34 +0200

    Update element.hpp

--- ships/element.hpp
+++ ships/element.hpp
@@ -1,19 +1,32 @@
-#pragma once
-
 #include <iostream>
 #include <vector>
 
 #ifndef ELEMENT_HPP
 #define ELEMENT_HPP
 
-struct
+struct 
 Element
 {
 
-    std::vector<std::pair<int, int>> elementCoordinates;
-    int health;
+    std::vector<std::pair<int, int>> m_elementCoordinates;
+    std::vector<std::pair<int, int>> m_hitCoordinates;
+    int m_health;
 
-    Element(std::vector<std::pair<int, int>> coordinates, int hlth) : elementCoordinates(coordinates), health(hlth) {}
+    Element(std::vector<std::pair<int, int>> coordinates, int hlth) : m_elementCoordinates(coordinates), m_health(hlth) {}
+
+    void 
+    setStateOfElement(const std::pair<int, int>& coordinates)
+    {
+
+        for (const auto& elementCoordinate : m_elementCoordinates)
+        {
+            if (elementCoordinate.first == coordinates.first && elementCoordinate.second == coordinates.second)
+            {
+                m_hitCoordinates.push_back(coordinates);
+            }
+        }
+
+    }
 
 };
 
